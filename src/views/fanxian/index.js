7ca55bfaafc373dc9ae942c29767262d24903dda
yangdecai1111 <461897887@qq.com>
2019-07-02 20:18:21 +0800

    'fanxian02'

--- src/views/fanxian/index.js
+++ src/views/fanxian/index.js
@@ -29,12 +29,126 @@ export default class Fanxian extends Component {
         </div>
         <section>
           <div className='m-header'>
-            <img src={require('../../images/zxt.jpg')} alt="" />
+            <img src={require('../../images/zxt.jpg')} alt="" className='img01'/>
             <img src={require('../../images/dianzhan.jpg')} alt="" />
             <h1>为你推荐</h1>
-            <img src={require('../../images/yxt.jpg')} alt="" />
+            <img src={require('../../images/yxt.jpg')} alt="" className='img01'/>
+            <p>你的口味,我都懂得</p>
           </div>
+          <div className='m-sales'>
+            <div className='sales-info'>
+              <img src="https://fuss10.elemecdn.com/1/a3/3d78e0783b5b74ea767855dd2a8a7jpeg.jpeg?imageMogr/format/webp/thumbnail/!345x345r/gravity/Center/crop/345x345/" alt=''/>
+              <h2>特色蜜汁烤肉拌饭</h2>
+              <div className='s-num'>
+                <span>月销170份</span>
+                <span>好评率94%</span>
+              </div>
+              <div className='s-price'>
+                <span className='price-info'>¥27.8</span>
+                <span className='price-yh'>满18减14</span>
+              </div>
+              <div className='sales-addr'>
+              <img src={require('../../images/sales.jpg')} alt="" />
+                <span>辣皇尚麻辣香锅(后瑞壹航城店)</span>
+              </div>
+            </div>
+            <div className='sales-info'>
+              <img src="https://fuss10.elemecdn.com/1/a3/3d78e0783b5b74ea767855dd2a8a7jpeg.jpeg?imageMogr/format/webp/thumbnail/!345x345r/gravity/Center/crop/345x345/" alt=''/>
+              <h2>特色蜜汁烤肉拌饭</h2>
+              <div className='s-num'>
+                <span>月销170份</span>
+                <span>好评率94%</span>
+              </div>
+              <div className='s-price'>
+                <span className='price-info'>¥27.8</span>
+                <span className='price-yh'>满18减14</span>
+              </div>
+              <div className='sales-addr'>
+                <img src={require('../../images/sales.jpg')} alt="" />
+                <span>辣皇尚麻辣香锅(后瑞壹航城店)</span>
+              </div>
+            </div>
+            <div className='sales-info'>
+              <img src="https://fuss10.elemecdn.com/1/a3/3d78e0783b5b74ea767855dd2a8a7jpeg.jpeg?imageMogr/format/webp/thumbnail/!345x345r/gravity/Center/crop/345x345/" alt=''/>
+              <h2>特色蜜汁烤肉拌饭</h2>
+              <div className='s-num'>
+                <span>月销170份</span>
+                <span>好评率94%</span>
+              </div>
+              <div className='s-price'>
+                <span className='price-info'>¥27.8</span>
+                <span className='price-yh'>满18减14</span>
+              </div>
+              <div className='sales-addr'>
+                <img src={require('../../images/sales.jpg')} alt="" />
+                <span>辣皇尚麻辣香锅(后瑞壹航城店)</span>
+              </div>
+            </div>
+            <div className='sales-info'>
+              <img src="https://fuss10.elemecdn.com/1/a3/3d78e0783b5b74ea767855dd2a8a7jpeg.jpeg?imageMogr/format/webp/thumbnail/!345x345r/gravity/Center/crop/345x345/" alt=''/>
+              <h2>特色蜜汁烤肉拌饭</h2>
+              <div className='s-num'>
+                <span>月销170份</span>
+                <span>好评率94%</span>
+              </div>
+              <div className='s-price'>
+                <span className='price-info'>¥27.8</span>
+                <span className='price-yh'>满18减14</span>
+              </div>
+              <div className='sales-addr'>
+              <img src={require('../../images/sales.jpg')} alt="" />
+                <span>辣皇尚麻辣香锅(后瑞壹航城店)</span>
+              </div>
+            </div>
+          </div>
+          <p className='more'>查看更多 ></p>
         </section>
+        <div className='giftbox'>
+          <div className='g-header'>
+              <img src={require('../../images/zxt.jpg')} alt="" className='img01'/>
+              <img src={require('../../images/naozhong.jpg')} alt="" />
+              <h1>限时好礼</h1>
+              <img src={require('../../images/yxt.jpg')} alt="" className='img01'/>
+              <p>金币换豪礼</p>
+            </div>
+          <div className='gitf-info'>
+            <div className='info-box'>
+              <div className='img-box'>
+                <span>限时优惠</span>
+                <img src="https://fuss10.elemecdn.com/5/78/5c1ca7ceca079926933ca418c9eb7jpeg.jpeg?imageMogr/format/webp/" alt='' />
+              </div>
+              <p>3元品质联盟红包</p>
+              <div className='food-price'>
+                <span className='price'>90金币</span>
+                <span className='price-you'>¥3</span>
+              </div>
+            </div>
+            <div className='info-box'>
+              <div className='img-box'>
+                <span>限时优惠</span>
+                <img src="https://fuss10.elemecdn.com/5/78/5c1ca7ceca079926933ca418c9eb7jpeg.jpeg?imageMogr/format/webp/" alt='' />
+              </div>
+              <p>3元品质联盟红包</p>
+              <div className='food-price'>
+                <span className='price'>90金币</span>
+                <span className='price-you'>¥3</span>
+              </div>
+            </div>
+            <div className='info-box'>
+              <div className='img-box'>
+                <span>限时优惠</span>
+                <img src="https://fuss10.elemecdn.com/5/78/5c1ca7ceca079926933ca418c9eb7jpeg.jpeg?imageMogr/format/webp/" alt='' />
+              </div>
+              <p>3元品质联盟红包</p>
+              <div className='food-price'>
+                <span className='price'>90金币</span>
+                <span className='price-you'>¥3</span>
+              </div>
+            </div>
+          </div>
+          <p className='more'>查看更多 ></p>
+        </div>
+        <p className='bot'></p>
       </div>
     )
   }
